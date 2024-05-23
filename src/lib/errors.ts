9a644cbfa4cd1ef068c2b939aea91164e1bc1e18
Mitch Downey <mitchldowney@gmail.com>
2024-05-22 20:53:47 -0500

    Update errors.ts

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -2,7 +2,7 @@ export const isError = (err: unknown): err is Error => err instanceof Error
 
 export const handleThrowError = (error: unknown) => {
   if (isError(error)) {
-    console.error('Error creating Image:', error.message)
+    console.error('Error:', error.message)
     throw new Error(error.message)
   } else {
     console.error('Error object has an invalid type', error)
